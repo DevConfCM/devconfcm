912c9ce9c8dc1330156ebc1f2bfe6e8e0de6e780
Ivange Larry <ivangelarry@gmail.com>
2022-02-17 10:17:23 +0100

    Refactor main.go

--- main.go
+++ main.go
@@ -1,17 +1,9 @@
 package main
 
 import (
-	"devconfcm/pkg/routes"
-
-	"github.com/labstack/echo/v4"
+	"devconfcm/cmd/devconfcm"
 )
 
 func main() {
-	e := echo.New()
-
-	routes.RegisterRoutes(e)
-
-	e.Static("static", "website/build/static")
-	e.File("/", "website/build/index.html")
-	e.Logger.Fatal(e.Start(":5000"))
+	devconfcm.Run()
 }
